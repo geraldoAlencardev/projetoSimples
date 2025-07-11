8f2eeb72251b311ff6d00765dad2f1ca367a5c92
Geraldo Alencar <geraldoalencardev@gmail.com>
2025-07-11 17:32:38 -0300

    Starting Project

---
+++ src/main/java/com/projetinho/projetoSimples/ProjetoSimplesApplication.java
@@ -0,0 +1,13 @@
+package com.projetinho.projetoSimples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoSimplesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoSimplesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projetinho/projetoSimples/ProjetoSimplesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projetinho.projetoSimples;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoSimplesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
